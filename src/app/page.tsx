81bdf330c4f85d0464ee83b2cd3c03434ed5ef59
SantiagoAmaya21 <santiago.amaya-z@mail.escuelaing.edu.co>
2025-05-24 15:10:27 -0500

    cambios estructura

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,8 +6,33 @@ import { useRouter } from 'next/navigation';
 export default function Home() {
     const router = useRouter();
 
+    /* const [role, setRole] = useState<string | null>(null);
+
+      useEffect(() => {
+        // Obtener rol desde localStorage
+        const storedRole = localStorage.getItem("role");
+        setRole(storedRole);
+      }, []);
+
+      const goToLoanScreen = () => {
+        if (role === "ADMIN") {
+          router.push("/sportLoans/adminPage");
+        } else if (role === "GENERALSERVICES") {
+          router.push("/sportLoans/generalServicesPage");
+        } else if (role === "USER") {
+          router.push("/sportLoans/userPage");
+        } else {
+          alert("Rol no reconocido o no estás logueado");
+        }
+      };
+
+    */
+
+
+
+
     const goToLoanScreen = () => {
-      router.push('/sportLoans');
+      router.push('/sportLoans/userPage');
     };
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
--- src/app/sportLoans/userPage/page.tsx
+++ src/app/sportLoans/userPage/page.tsx
@@ -4,13 +4,13 @@ import LoanForm from '@/components/LoanForm';
 import ReturnForm from '@/components/ReturnForm';
 import EquipmentStatusForm from '@/components/EquipmentStatusForm';
 import EquipmentList from '@/components/EquipmentList';
-import Notification from '@/components/Notification'
-import UserLoansList from '@/components/UserLoansList'
-import EquipmentById from '@/components/EquipmentById'
-import AddEquipmentForm from '@/components/AddEquipmentForm'
+import Notification from '@/components/Notification';
+import UserLoansList from '@/components/UserLoansList';
+import EquipmentById from '@/components/EquipmentById';
+import AddEquipmentForm from '@/components/AddEquipmentForm';
 
 
-export default function HomePage() {
+export default function userPage() {
   return (
     <main className="max-w-4xl mx-auto p-4 space-y-6">
       <h1 className="text-3xl font-bold mb-4">Gestión de Préstamos Deportivos</h1>
