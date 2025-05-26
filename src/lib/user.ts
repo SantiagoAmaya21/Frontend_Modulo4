55e2c3ccdbad776e2be3ee99bb4c09760e143672
SantiagoAmaya21 <santiago.amaya-z@mail.escuelaing.edu.co>
2025-05-25 22:06:41 -0500

    fix

--- src/lib/user.ts
+++ src/lib/user.ts
@@ -2,6 +2,12 @@ import axios from 'axios';
 
 const API_URL = 'https://moduloadmin-b8e8a8c9apcfcyb3.brazilsouth-01.azurewebsites.net//api/usuarios';
 
+const getAuthHeaders = () => {
+  const token = localStorage.getItem('token');
+  return {
+    Authorization: `Bearer ${token}`,
+  };
+};
 
 export async function loginUser(correo: string, contraseña: string): Promise<string> {
   try {
@@ -17,7 +23,9 @@ export async function loginUser(correo: string, contraseña: string): Promise<st
 
 export async function getCorreoById(id: string): Promise<string> {
   try {
-    const response = await axios.get(`${API_URL}/correo/${id}`);
+    const response = await axios.get(`${API_URL}/correo/${id}`, {
+        headers: getAuthHeaders(),
+    });
     return response.data;
   } catch (error: any) {
     throw new Error(error.response?.data || 'Error al obtener el correo del usuario');
