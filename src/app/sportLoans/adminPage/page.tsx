b6553f8980cefb9a617865646b5c88d1a2ca9d84
SantiagoAmaya21 <santiago.amaya-z@mail.escuelaing.edu.co>
2025-05-25 23:25:41 -0500

    Fix

--- src/app/sportLoans/adminPage/page.tsx
+++ src/app/sportLoans/adminPage/page.tsx
@@ -1,5 +1,7 @@
 'use client';
 
+import React, { useState } from 'react';
+
 import AddEquipmentForm from '@/components/AddEquipmentForm';
 import ReturnForm from '@/components/ReturnForm';
 import EquipmentStatusForm from '@/components/EquipmentStatusForm';
